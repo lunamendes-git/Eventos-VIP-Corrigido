056389489545932ebbb7a41a5d3fb884b6dcd5f4
luna.mendes <luna.mendes@somosicev.com>
2025-12-01 10:07:36 -0300

    Criação de classes em entidade

--- src/entidade/Convidado.java
+++ src/entidade/Convidado.java
@@ -1,2 +1,52 @@
 package entidade;
 
+import Desconto.Desconto;
+import Desconto.DescontoRegular;
+import Desconto.DescontoVIP;
+
+public class Convidado extends Pessoa {
+    private String tipo;
+    private Desconto estrategiaDesconto;
+
+    public Convidado(Integer id, String nome, String tipo) {
+        super(id, nome);
+        this.tipo = tipo.toUpperCase();
+
+
+        if (!this.tipo.equals("VIP") && !this.tipo.equals("REGULAR")) {
+            throw new IllegalArgumentException("Tipo de convidado inválido: " + tipo);
+        }
+
+        if (this.tipo.equals("VIP")) {
+            this.estrategiaDesconto = new DescontoVIP();
+        } else {
+            this.estrategiaDesconto = new DescontoRegular();
+        }
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public boolean isVIP() {
+        return tipo.equals("VIP");
+    }
+
+    public double calcularValorFinal(double valor) {
+        double desconto = estrategiaDesconto.calcularDesconto(valor);
+        return valor - desconto;
+    }
+
+    public double getValorDesconto(double valor) {
+        return estrategiaDesconto.calcularDesconto(valor);
+    }
+
+    public String getDescricaoDesconto() {
+        return estrategiaDesconto.getDescricao();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Tipo: " + tipo + ", " + estrategiaDesconto.getDescricao();
+    }
+}
---
+++ src/entidade/Pessoa.java
@@ -0,0 +1,28 @@
+package entidade;
+
+public abstract class Pessoa {
+    protected Integer id;
+    protected String nome;
+
+    public Pessoa(Integer id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id + ", Nome: " + nome;
+    }
+}
