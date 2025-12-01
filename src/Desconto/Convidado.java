520e2053c237ee873bb000dc4975fe867539599c
luna.mendes <luna.mendes@somosicev.com>
2025-12-01 09:50:38 -0300

    Criação das Classes Convidado

---
+++ src/Desconto/Convidado.java
@@ -0,0 +1,36 @@
+package Desconto;
+public class Convidado {
+    private Integer id;
+    private String nome;
+    private String tipo; // VIP ou regular
+
+    public Convidado(Integer id, String nome, String tipo) {
+        this.id = id;
+        this.nome = nome;
+        this.tipo = tipo;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+}
